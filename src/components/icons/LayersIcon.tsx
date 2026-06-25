4a8bfd4ee0b1725773b7d7f2c6cb63db321d18d7
awdrrawd <awdrrawd1@gmail.com>
2026-06-25 17:31:39 +0800

    add return

--- src/components/icons/LayersIcon.tsx
+++ src/components/icons/LayersIcon.tsx
@@ -1,8 +1,5 @@
 export function LayersIcon() {
-  return <svg width="16" height="16" viewBox="0 0 14 14" fill="none" stroke="currentColor" strokeWidth="1.5"
-              strokeLinecap="round">
-    <rect x="1" y="1" width="12" height="3" rx="1"/>
-    <rect x="1" y="5.5" width="12" height="3" rx="1"/>
-    <rect x="1" y="10" width="12" height="3" rx="1"/>
+  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="17" height="17" fill="currentColor">
+    <path d="m0 0h127v36h37v37h36v127h-127v-36h-37v-37h-36zm17.83 64.17l0.17 44.83h91v-91c-69.49 0-90.04 0.3-90.5 0.67-0.46 0.36-0.76 20.84-0.67 45.5zm146.17 99.83h-73v18l90.5-0.5 0.51-90.5h-18.01z"/>
   </svg>;
 }
